1d0bf8bf7385682606b9de610573096c580bfa4e
Rogério Santos <rogeriosantosdev@gmail.com>
2024-01-14 21:44:24 -0300

    first commit

---
+++ EmployeeRegister/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace EmployeeRegister
+{
+    internal class Employee(int id, string name, double salary)
+    {
+        public int Id => id;
+        public string Name => ValidateNameLenght(name);
+        public double Salary => salary;
+
+        public double IncreaseSalary(double percentage)
+        {
+            return salary += salary * (percentage / 100);
+        }
+
+        private string? ValidateNameLenght(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return null;
+            }
+            if (name.Length > 1)
+            {
+                return name;
+            }
+            return name;
+        }
+
+        public override string ToString()
+        {
+            return "Name: "+Name+ " ID: " + id + " Salary: R$"+salary;
+        }
+
+    }
+}
---
+++ EmployeeRegister/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace EmployeeRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string name;
+            double salary;
+
+            Console.Write("How many employee will be registred: ");
+            int qtdEmployee = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new();
+
+            for(int i = 0; i < qtdEmployee; i++)
+            {
+                Console.Write("ID: ");
+                id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                salary = int.Parse(Console.ReadLine());
+
+                Employee employee = new(id, name, salary);
+                list.Add(employee);
+            }
+
+            Console.WriteLine("Enter the employee id that will be salary increase");
+            int employeeID = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the percentage");
+            double percentage = double.Parse(Console.ReadLine());
+            Employee employee1 = verifyID(employeeID, list);
+
+            if (employee1 == null)
+            {
+                Console.WriteLine("This id doesn't exist!");
+            } else
+            {
+                employee1.IncreaseSalary(percentage);
+            }
+
+            Console.WriteLine(employee1);
+        }
+
+        public static Employee verifyID(int id, List<Employee> list)
+        {
+            foreach (Employee employeeid in list)
+            {
+                return list.Find(x => x.Id == id);
+            }
+
+            return null;
+        }
+    }
+}
